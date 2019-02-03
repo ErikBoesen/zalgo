ebcf49391a46f2a51d2ec2a706bf4aa5401ae542
ErikBoesen <me@erikboesen.com>
2019-02-03 11:26:00 -0500

    Shorten minimum and maximum

--- zalgo/main.py
+++ zalgo/main.py
@@ -4,7 +4,7 @@ DIACRITICS_BOTTOM = ['̖',' ̗',' ̘',' ̙',' ̜',' ̝',' ̞',' ̟',' ̠',' ̤',
 DIACRITICS_TOP = [' ̍',' ̎',' ̄',' ̅',' ̿',' ̑',' ̆',' ̐',' ͒',' ͗',' ͑',' ̇',' ̈',' ̊',' ͂',' ̓',' ̈́',' ͊',' ͋',' ͌',' ̃',' ̂',' ̌',' ͐',' ́',' ̋',' ̏',' ̽',' ̉',' ͣ',' ͤ',' ͥ',' ͦ',' ͧ',' ͨ',' ͩ',' ͪ',' ͫ',' ͬ',' ͭ',' ͮ',' ͯ',' ̾',' ͛',' ͆',' ̚',]
 DIACRITICS_MIDDLE = [' ̕',' ̛',' ̀',' ́',' ͘',' ̡',' ̢',' ̧',' ̨',' ̴',' ̵',' ̶',' ͜',' ͝',' ͞',' ͟',' ͠',' ͢',' ̸',' ̷',' ͡',]
 
-def process(text: str, minimum_bottom_diacritics=1, maximum_bottom_diacritics=3, minimum_middle_diacritics=1, maximum_middle_diacritics=2, minimum_top_diacritics=1, maximum_top_diacritics=3, maximum_diacritics_per_letter=3):
+def process(text: str, min_bottom_diacritics=1, max_bottom_diacritics=3, min_middle_diacritics=1, max_middle_diacritics=2, min_top_diacritics=1, max_top_diacritics=3, max_diacritics_per_letter=3):
     """
     Add zalgo characters to a string.
     """
@@ -18,28 +18,28 @@ def process(text: str, minimum_bottom_diacritics=1, maximum_bottom_diacritics=3,
             output += letter
             continue
 
-        num_accents = 0
-        num_top_accents = random.randint(minimum_top_diacritics,maximum_top_diacritics)
-        num_bottom_accents = random.randint(minimum_bottom_diacritics,maximum_bottom_diacritics)
-        num_middle_accents = random.randint(minimum_middle_diacritics,maximum_middle_diacritics)
+        accents_added = 0
+        num_top_accents = random.randint(min_top_diacritics,max_top_diacritics)
+        num_bottom_accents = random.randint(min_bottom_diacritics,max_bottom_diacritics)
+        num_middle_accents = random.randint(min_middle_diacritics,max_middle_diacritics)
         # Add diacritics randomly to letter until saturation
-        while num_accents < maximum_diacritics_per_letter and num_top_accents + num_middle_accents + num_bottom_accents != 0:
+        while accents_added < max_diacritics_per_letter and num_top_accents + num_middle_accents + num_bottom_accents != 0:
             randint = random.randint(0,2) # randomly choose what accent type to add
             if randint == 0:
                 if num_top_accents > 0:
                     letter = combine_with_diacritic(letter, DIACRITICS_TOP)
-                    num_accents += 1
+                    accents_added += 1
                     num_top_accents -= 1
             elif randint == 1:
                 if num_bottom_accents > 0:
                     letter = combine_with_diacritic(letter, DIACRITICS_BOTTOM)
                     num_bottom_accents -= 1
-                    num_accents += 1
+                    accents_added += 1
             else:
                 if num_middle_accents > 0:
                     letter = combine_with_diacritic(letter, DIACRITICS_MIDDLE)
                     num_middle_accents -= 1
-                    num_accents += 1
+                    accents_added += 1
 
         #a = a.replace(" ","") #remove any spaces, this also gives it the zalgo text look
         output += letter
