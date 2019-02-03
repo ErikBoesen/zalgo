b99620470db372060967e0ce9468f02022cfbd4a
ErikBoesen <me@erikboesen.com>
2019-02-03 11:52:20 -0500

    Don't randomize too many times

--- zalgo/main.py
+++ zalgo/main.py
@@ -1,10 +1,12 @@
 import random
+from unidecode import unidecode
+
 
 DIACRITICS_TOP = [' ̍',' ̎',' ̄',' ̅',' ̿',' ̑',' ̆',' ̐',' ͒',' ͗',' ͑',' ̇',' ̈',' ̊',' ͂',' ̓',' ̈́',' ͊',' ͋',' ͌',' ̃',' ̂',' ̌',' ͐',' ́',' ̋',' ̏',' ̽',' ̉',' ͣ',' ͤ',' ͥ',' ͦ',' ͧ',' ͨ',' ͩ',' ͪ',' ͫ',' ͬ',' ͭ',' ͮ',' ͯ',' ̾',' ͛',' ͆',' ̚',]
 DIACRITICS_MIDDLE = [' ̕',' ̛',' ̀',' ́',' ͘',' ̡',' ̢',' ̧',' ̨',' ̴',' ̵',' ̶',' ͜',' ͝',' ͞',' ͟',' ͠',' ͢',' ̸',' ̷',' ͡',]
 DIACRITICS_BOTTOM = ['̖',' ̗',' ̘',' ̙',' ̜',' ̝',' ̞',' ̟',' ̠',' ̤',' ̥',' ̦',' ̩',' ̪',' ̫',' ̬',' ̭',' ̮',' ̯',' ̰',' ̱',' ̲',' ̳',' ̹',' ̺',' ̻',' ̼',' ͅ',' ͇',' ͈',' ͉',' ͍',' ͎',' ͓',' ͔',' ͕',' ͖',' ͙',' ͚',' ',]
 
-def process(text: str, min_top_diacritics=1, max_top_diacritics=3, min_middle_diacritics=1, max_middle_diacritics=2, min_bottom_diacritics=1, max_bottom_diacritics=3, max_diacritics_per_character=3, remove_spaces=False):
+def process(text: str, min_top_diacritics=1, max_top_diacritics=3, min_middle_diacritics=1, max_middle_diacritics=2, min_bottom_diacritics=1, max_bottom_diacritics=3, remove_spaces=False):
     """
     Add zalgo characters to a string.
     """
@@ -17,28 +19,15 @@ def process(text: str, min_top_diacritics=1, max_top_diacritics=3, min_middle_di
             output += character
             continue
 
-        diacritics_added = 0
-        num_top_diacritics = random.randint(min_top_diacritics,max_top_diacritics)
-        num_bottom_diacritics = random.randint(min_bottom_diacritics,max_bottom_diacritics)
-        num_middle_diacritics = random.randint(min_middle_diacritics,max_middle_diacritics)
-        # Add diacritics randomly to character until saturation
-        while diacritics_added < max_diacritics_per_character and num_top_diacritics + num_middle_diacritics + num_bottom_diacritics != 0:
-            randint = random.randint(0,2) # randomly choose what accent type to add
-            if randint == 0:
-                if num_top_diacritics > 0:
-                    character = combine_with_diacritic(character, DIACRITICS_TOP)
-                    diacritics_added += 1
-                    num_top_diacritics -= 1
-            elif randint == 1:
-                if num_middle_diacritics > 0:
-                    character = combine_with_diacritic(character, DIACRITICS_MIDDLE)
-                    num_middle_diacritics -= 1
-                    diacritics_added += 1
-            else:
-                if num_bottom_diacritics > 0:
-                    character = combine_with_diacritic(character, DIACRITICS_BOTTOM)
-                    num_bottom_diacritics -= 1
-                    diacritics_added += 1
+        num_top_diacritics = random.randint(min_top_diacritics, max_top_diacritics)
+        num_bottom_diacritics = random.randint(min_bottom_diacritics, max_bottom_diacritics)
+        num_middle_diacritics = random.randint(min_middle_diacritics, max_middle_diacritics)
+        for i in range(num_top_diacritics):
+            character = mark(character, DIACRITICS_TOP)
+        for i in range(num_middle_diacritics):
+            character = mark(character, DIACRITICS_MIDDLE)
+        for i in range(num_bottom_diacritics):
+            character = mark(character, DIACRITICS_BOTTOM)
 
         output += character
 
@@ -47,7 +36,7 @@ def process(text: str, min_top_diacritics=1, max_top_diacritics=3, min_middle_di
 
     return output
 
-def combine_with_diacritic(character, diacritic_options):
+def mark(character, diacritic_options):
     """
     Combine character with a random diacritic.
     """
