d96663f5b7529ad3b3484670e3545b58a8d59a77
ErikBoesen <me@erikboesen.com>
2019-02-03 11:00:22 -0500

    Get rid of camel case

--- zalgo/zalgo.py
+++ zalgo/zalgo.py
@@ -1,62 +1,61 @@
 import random
 
-DIACRITICS_DOWNWARD = ['̖',' ̗',' ̘',' ̙',' ̜',' ̝',' ̞',' ̟',' ̠',' ̤',' ̥',' ̦',' ̩',' ̪',' ̫',' ̬',' ̭',' ̮',' ̯',' ̰',' ̱',' ̲',' ̳',' ̹',' ̺',' ̻',' ̼',' ͅ',' ͇',' ͈',' ͉',' ͍',' ͎',' ͓',' ͔',' ͕',' ͖',' ͙',' ͚',' ',]
-DIACRITICS_UPWARD = [' ̍',' ̎',' ̄',' ̅',' ̿',' ̑',' ̆',' ̐',' ͒',' ͗',' ͑',' ̇',' ̈',' ̊',' ͂',' ̓',' ̈́',' ͊',' ͋',' ͌',' ̃',' ̂',' ̌',' ͐',' ́',' ̋',' ̏',' ̽',' ̉',' ͣ',' ͤ',' ͥ',' ͦ',' ͧ',' ͨ',' ͩ',' ͪ',' ͫ',' ͬ',' ͭ',' ͮ',' ͯ',' ̾',' ͛',' ͆',' ̚',]
+DIACRITICS_BOTTOM = ['̖',' ̗',' ̘',' ̙',' ̜',' ̝',' ̞',' ̟',' ̠',' ̤',' ̥',' ̦',' ̩',' ̪',' ̫',' ̬',' ̭',' ̮',' ̯',' ̰',' ̱',' ̲',' ̳',' ̹',' ̺',' ̻',' ̼',' ͅ',' ͇',' ͈',' ͉',' ͍',' ͎',' ͓',' ͔',' ͕',' ͖',' ͙',' ͚',' ',]
+DIACRITICS_TOP = [' ̍',' ̎',' ̄',' ̅',' ̿',' ̑',' ̆',' ̐',' ͒',' ͗',' ͑',' ̇',' ̈',' ̊',' ͂',' ̓',' ̈́',' ͊',' ͋',' ͌',' ̃',' ̂',' ̌',' ͐',' ́',' ̋',' ̏',' ̽',' ̉',' ͣ',' ͤ',' ͥ',' ͦ',' ͧ',' ͨ',' ͩ',' ͪ',' ͫ',' ͬ',' ͭ',' ͮ',' ͯ',' ̾',' ͛',' ͆',' ̚',]
 DIACRITICS_MIDDLE = [' ̕',' ̛',' ̀',' ́',' ͘',' ̡',' ̢',' ̧',' ̨',' ̴',' ̵',' ̶',' ͜',' ͝',' ͞',' ͟',' ͠',' ͢',' ̸',' ̷',' ͡',]
 
-def process(self, text: str, downward_diacritics_count=(1, 3), middle_diacritics_count=(1, 2), upward_diacritics_count=(1, 3), maximum_diacritics_per_letter=3):
+def process(text: str, bottom_diacritics_count=(1, 3), middle_diacritics_count=(1, 2), top_diacritics_count=(1, 3), maximum_diacritics_per_letter=3):
     """
     Add zalgo characters to a string.
     """
-    #get the letters list
-    letters = list(text) #['t','e','s','t',' ','t',...]
-    #print(letters)
-    newWord = ''
-    newLetters = []
+    # Split string into its constituent characters
+    letters = list(text)
+    new_word = ''
+    new_letters = []
 
-    #for each letter, add some diacritics of all varieties
-    for letter in letters: #'p', etc...
+    # Add diacritics of each type for each letter
+    for letter in letters:
         a = letter #create a dummy letter
 
         #skip this letter we can't add a diacritic to it
         if not a.isalpha():
-            newLetters.append(a)
+            new_letters.append(a)
             continue
 
-        numAccents = 0
-        numU = random.randint(upward_diacritics_count[0],upward_diacritics_count[1])
-        numD = random.randint(downward_diacritics_count[0],downward_diacritics_count[1])
+        num_accents = 0
+        numU = random.randint(top_diacritics_count[0],top_diacritics_count[1])
+        numD = random.randint(bottom_diacritics_count[0],bottom_diacritics_count[1])
         numM = random.randint(middle_diacritics_count[0],middle_diacritics_count[1])
         #Try to add accents to the letter, will add an upper, lower, or middle accent randomly until
-        #either numAccents == maximum_diacritics_per_letter or we have added the maximum upper, middle and lower accents. Denoted
+        #either num_accents == maximum_diacritics_per_letter or we have added the maximum upper, middle and lower accents. Denoted
         #by numU, numD, and numM
-        while numAccents < maximum_diacritics_per_letter and numU + numM + numD != 0:
+        while num_accents < maximum_diacritics_per_letter and numU + numM + numD != 0:
             randint = random.randint(0,2) # randomly choose what accent type to add
             if randint == 0:
                 if numU > 0:
-                    a = self.combine_with_diacritic(a, DIACRITICS_UPWARD)
-                    numAccents += 1
+                    a = self.combine_with_diacritic(a, DIACRITICS_TOP)
+                    num_accents += 1
                     numU -= 1
             elif randint == 1:
                 if numD > 0:
-                    a = self.combine_with_diacritic(a, DIACRITICS_DOWNWARD)
+                    a = self.combine_with_diacritic(a, DIACRITICS_BOTTOM)
                     numD -= 1
-                    numAccents += 1
+                    num_accents += 1
             else:
                 if numM > 0:
                     a = self.combine_with_diacritic(a, DIACRITICS_MIDDLE)
                     numM -= 1
-                    numAccents += 1
+                    num_accents += 1
 
         #a = a.replace(" ","") #remove any spaces, this also gives it the zalgo text look
         #print('accented a letter: ' + a)
-        newLetters.append(a)
+        new_letters.append(a)
 
-    newWord = ''.join(newLetters)
-    return newWord
+    new_word = ''.join(new_letters)
+    return new_word
 
-def combine_with_diacritic(self, letter, diacriticList):
-    '''
-    Combines letter and a random character from diacriticList
-    '''
-    return letter.strip() + diacriticList[random.randrange(0, len(diacriticList))].strip()
+def combine_with_diacritic(letter, diacritic_options):
+    """
+    Combine letter with a random diacritic.
+    """
+    return letter.strip() + diacritic_options[random.randrange(0, len(diacritic_options))].strip()
