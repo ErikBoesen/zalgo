03608635a7fd0d98d0290f7196ed5d5a6ad952fa
ErikBoesen <me@erikboesen.com>
2019-02-03 11:59:40 -0500

    Stop requiring unidecode

--- setup.py
+++ setup.py
@@ -9,6 +9,5 @@ setup(
     author_email = 'me@erikboesen.com',
     url = 'https://github.com/ErikBoesen/zalgo',
     keywords = ['diacritic', 'filter', 'zalgo', 'he is coming', 'ahhh'],
-    install_requires=['unidecode'],
     classifiers = [],
 )
