2eb31bb9740f110aca52b0826bde889c2ddef7ff
ErikBoesen <me@erikboesen.com>
2019-02-04 21:46:22 -0500

    Bump to 1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'zalgoify',
     packages = ['zalgoify'], # this must be the same as the name above
-    version = '1.0',
+    version = '1.1',
     description = 'A simple library for processing zalgo text',
     author = 'Erik Boesen',
     author_email = 'me@erikboesen.com',
